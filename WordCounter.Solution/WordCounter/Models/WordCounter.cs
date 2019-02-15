bb982ede775c6295f3254401bca37bdf9ab4c3f9
ThearyIm <thearyim@gmail.com>
2019-02-15 10:51:23 -0800

    Add WordCounter logic

--- WordCounter.Solution/WordCounter/Models/WordCounter.cs
+++ WordCounter.Solution/WordCounter/Models/WordCounter.cs
@@ -4,21 +4,40 @@ namespace WordCount
 {
   public class WordCounter
   {
-    public string GetWordCount(int number)
+    public string GetWordCount(string word)
     {
-      //
+      //gathers both a word and sentence from a user
+      //then checks how frequently the word appears
+      //in the sentence
+      //i.e. Put the garbage out for the garbage truck
+      //the: 2
+      //garbage: 2
+      //put: 2
 
-      //
-      string[] numberChar = number.ToString().Split();
+      ContainsKey(string);
+      sentence =;
+      //create a dictionary which takes in strings and int
+      Dictionary<string, int>//(StringCompare.OrdinalIgnorCase)
 
-      //
-      List<string> words = new List<string>();
+      //split sentence into strings of words
+      string[] words = sentence.Split(stringSplitOption.RemoveEmptyEntries)
 
+      //forEach word in words
+      wordLc = word.ToLower()
+      if dictionary.ContainsKey(wordLc)
+      {
+        dictionary[wordLc]++;
+      }
+      else
+      {
+        dictionary.Add(wordLc, 1)
+      }
 
-
-
-
-
+      //KeyValuePair<string, int>
+      forEach(var entry in dictionary)
+      {
+        Output entry.Key(word)Count;
+      }
 
     }
   }
